493ac76bda4234a3894cda7b385f793c36b0df97
RyanKilleen <46888783+RyanKilleen@users.noreply.github.com>
2019-11-12 14:37:24 +0000

    Add project files.

---
+++ Lab Sheet 7/Expense.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Sheet_7
+{
+     public class Expense
+    {
+        //Properties
+        public decimal Amount { get; set; }
+        public DateTime ExpenseDate { get; set; }
+        public string Category { get; set; }
+
+        //Construtors
+
+        //Methods
+
+    }
+}
